bd5f44ec2b74516f0d2e149941863d7612ff05c0
neoskyneomap <neoskyneomap777@gmail.com>
2024-05-21 17:13:24 +0900

    configのimportを追加

--- train_cycle.py
+++ train_cycle.py
@@ -9,20 +9,11 @@ from self_play import self_play
 from train_network import train_network
 from evaluate_network import evaluate_network
 #from evaluate_best_player import evaluate_best_player
+import config
 
-# GPUの設定を確認する
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        # 設定により、必要なメモリのみを確保するように設定
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # メモリ設定はプログラム開始前に行う必要があるため、エラーが発生する可能性があります。
-        print(e)
-        
+# GPU設定の読み込み
+config.setup_gpu()
+
 # デュアルネットワークの作成
 dual_network()
 
