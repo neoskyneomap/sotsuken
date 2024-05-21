3d3dbf4b200e535b94ac80b7d83b5b879eeacb91
neoskyneomap <neoskyneomap777@gmail.com>
2024-05-21 17:40:46 +0900

    バッチサイズを256から128

--- train_network.py
+++ train_network.py
@@ -57,7 +57,7 @@ def train_network():
                 print('\rTrain {}/{}'.format(epoch + 1,RN_EPOCHS), end=''))
 
     # 学習の実行
-    model.fit(xs, [y_policies, y_values], batch_size=256, epochs=RN_EPOCHS,
+    model.fit(xs, [y_policies, y_values], batch_size=128, epochs=RN_EPOCHS,
             verbose=0, callbacks=[lr_decay, print_callback])
     print('')
 
